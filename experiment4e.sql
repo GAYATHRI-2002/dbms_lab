80e3efad85736c56895defd4eda3e3d5c417b504
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-01-03 19:53:56 +0530

    Update experiment4e.sql

--- experiment4e.sql
+++ experiment4e.sql
@@ -37,3 +37,45 @@ mysql> show index from book_issue;
 | book_issue |          1 | abc      |            2 | book_id     | A         |           0 |     NULL |   NULL | YES  | BTREE      |         |               | YES     | NULL       |
 +------------+------------+----------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
 4 rows in set (0.00 sec)
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+create and execute sql commands to insert data into each of the tables designed.
+
+mysql> desc book;
++----------------+-------------+------+-----+---------+-------+
+| Field          | Type        | Null | Key | Default | Extra |
++----------------+-------------+------+-----+---------+-------+
+| book_id        | int         | NO   | PRI | NULL    |       |
+| title          | varchar(20) | YES  |     | NULL    |       |
+| lang_id        | int         | YES  |     | NULL    |       |
+| mrp            | int         | YES  |     | NULL    |       |
+| publisher_id   | int         | YES  |     | NULL    |       |
+| published_date | date        | YES  |     | NULL    |       |
+| volume         | int         | YES  |     | NULL    |       |
+| status         | varchar(20) | YES  |     | NULL    |       |
++----------------+-------------+------+-----+---------+-------+
+8 rows in set (0.00 sec)
+
+mysql> select *from book;
+Empty set (0.01 sec)
+
+mysql> insert into book values( 101,'hidden world',1,100,01,'1-2-13',2,'yes');
+Query OK, 1 row affected (0.01 sec)
+
+mysql> insert into book values(102,'good day',3,200,02,'2-3-14',4,'yes');
+Query OK, 1 row affected (0.01 sec)
+
+mysql> select *from book;
++---------+--------------+---------+------+--------------+----------------+--------+--------+
+| book_id | title        | lang_id | mrp  | publisher_id | published_date | volume | status |
++---------+--------------+---------+------+--------------+----------------+--------+--------+
+|     101 | hidden world |       1 |  100 |            1 | 0001-02-13     |      2 | yes    |
+|     102 | good day     |       3 |  200 |            2 | 0002-03-14     |      4 | yes    |
++---------+--------------+---------+------+--------------+----------------+--------+--------+
+2 rows in set (0.00 sec)
+
+
+
